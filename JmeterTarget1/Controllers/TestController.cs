3af154bae1fab671188b591b8a39e13a651e7c48
nakamura132 <windpublic@gmail.com>
2024-08-05 22:05:10 +0900

    JMeterテスト計画動作確認用プロジェクトJmeterTarget1を追加 最初は小さいメッセージを返すだけの1つのエンドポイントのみが存在する

---
+++ JmeterTarget1/Controllers/TestController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace JmeterTarget1.Controllers
+{
+    [Route( "api/[controller]" )]
+    [ApiController]
+    public class TestController : ControllerBase
+    {
+        [HttpGet]
+        public async Task<IActionResult> Get()
+        {
+            // レスポンスデータを作成
+            var data = new
+            {
+                Message = "Server response.",
+                Timestamp = DateTime.UtcNow.ToString("yyyy-MM-ddTHH:mm:ssZ")
+            };
+
+            return Ok( data ); // 200 OK を返す
+        }
+    }
+}
